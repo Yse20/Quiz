43dde40320d2fdbd173063ab8a8432380d56176a
Yse20 <kimyse80@gmail.com>
2024-11-19 12:09:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,50 @@ result. The function should be called concatenate(char *str1, char *str2) and re
 */
 #include <stdio.h>
 
-int main()
-{
-    //write your code here.
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+char* concatenate(char *str1, char *str2) {
+    int count1 = 0, count2 = 0;
+    char *ptr1 = str1, *ptr2 = str2;
+
+    while (*ptr1++) count1++; // count the number of letters
+    while (*ptr2++) count2++;
+
+    char *result = (char *)malloc(count1 + count2 + 1); // dynamically allocate memory to hold the concatenated 
+
+    char *answer = result;
+    while (*str1) {
+        *answer++ = *str1++; // append first str
+    }
+
+    while (*str2) {
+        *answer++ = *str2++; // append second str
+    }
+
+    *answer = '\0'; // append null 
+
+    return result;
+}
+
+int main() {
+
+    char str1[MAX];
+    char str2[MAX];
+
+    printf("Write the first string : "); // get the first string
+    scanf("%s", str1);
+    printf("Write the second string : "); // get the second string
+    scanf("%s", str2);   
+    
+    char *result = concatenate(str1, str2);
+
+    if (result != NULL) {
+        printf("\nConcatenated String: %s\n", result);
+        free(result);
+    }
 
     return 0;
-
 }
